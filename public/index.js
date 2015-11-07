ee468eb76e1c185748b9552e72345c47eadba084
arbass22 <arbass@umass.edu>
2015-11-07 03:16:55 -0500

    fixed countdown

--- public/index.js
+++ public/index.js
@@ -4,13 +4,19 @@ var socket = io.connect('http://localhost:3000');
 console.log('Index.js called');
 
 socket.on('countdown', function(data) {
-  for (var i = data.count; i >= 0; i--) {
-    setTimeout(function() {
-      document.getElementById('count-box').innerHTML = i;
-    }, 1000);
+  for (var i = 1; i <= 4; i++) {
+    setTimeout(function(i) {
+      document.getElementById('count-box').innerHTML = 4 - i
+    }, i * 1000)
   }
-  // collectData();
-  console.log("Countdown achieved");
+  setTimeout(collectData(), 5000);
+  // for (var i = data.count; i >= 0; i--) {
+  //   setTimeout(function(i) {
+  //     document.getElementById('count-box').innerHTML = i;
+  //   }, 1000);
+  // }
+  // // collectData();
+  // setTimeout(console.log("Countdown achieved");, 4000);
 });
 
 function collectData() {
