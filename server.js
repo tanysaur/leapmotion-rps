ffbf41c23a7b7b2f6cbdfb9d9ea4b4617f32668d
Justin Kennedy <jekennedy@umass.edu>
2015-11-06 21:07:30 -0500

    node setup

---
+++ server.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(){
+  console.log("AYY LMAO");
+});
+
+
+
+server.listen(3000);
