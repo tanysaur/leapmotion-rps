59519bfe234f96a65e3cafe4e0ae70ad89413276
arbass22 <arbass@umass.edu>
2015-11-06 22:48:33 -0500

    Include socket io

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
 
 app.use(express.static('public'));
 
-var server = app.listen(3000, function () {
-  console.log('started');
-});
+// io.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
