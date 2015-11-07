326c73fe6652a1c5fd52e7bd62b57f6fd7158755
Justin Kennedy <jekennedy@umass.edu>
2015-11-06 21:32:22 -0500

    port 80

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+app.use(express.static('public'));
+
 io.on('connection', function(){
   console.log("AYY LMAO");
 });
 
 
 
-server.listen(3000);
+server.listen(80);
