95e004102f52b2597ed8ed602e7e861bea23c389
Alfonso Merkl <anmerkl@gmail.com>
2015-11-06 23:36:24 -0500

    Added countdown to Game

--- games/RPS.js
+++ games/RPS.js
@@ -1,7 +1,12 @@
-var Game = function(player1, player2){
+var Game = function(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
-  console.log('New Game created with' + this.player1 + 'and' + this.player2);
+  this.sendCountdown(player1, 3);
+  this.sendCountdown(player2, 3);
+};
+
+Game.prototype.sendCountdown = function(player, count) {
+  player.emit('countdown', {count: count});
 };
 
 module.exports = Game;
